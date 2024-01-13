ad06105399381948ab1f25f42429dba49877fec7
melindakhosasih <melindakhosasih@gmail.com>
2024-01-14 05:33:45 +0800

    zero cost indexing done

--- Example/project.cpp
+++ Example/project.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <math.h>
 #include <vector>
+#include <set>
 
 using namespace std;
 
@@ -114,7 +115,7 @@ void run_LSB(ifstream& fin, ofstream& fout, int tag_cnt, int index_cnt, int n_wa
     print_out(fout, "Total cache miss count: ", total_miss);
 }
 
-void run_zero_cost(ifstream& fin, ofstream& fout, int index_cnt, int len, int n_way) {
+void run_zero_cost(ifstream& fin, ofstream& fout, int index_cnt, int offset_cnt, int len, int n_way) {
     // cout << "tag bit " << tag_cnt << endl;
     // cout << "index bit " << index_cnt << endl;
     // cout << "sets " << (1 << index_cnt) << endl;
@@ -151,7 +152,8 @@ void run_zero_cost(ifstream& fin, ofstream& fout, int index_cnt, int len, int n_
 
     // Calculate Quality Measurement (Q)
     // Count zero and one
-    int zero[len] = {0}, one[len] = {0}, Q[len];
+    int zero[len] = {0}, one[len] = {0};
+    double Q[len];
     for (int i = 0; i < addresses.size(); i++) {
         for (int j = 0; j < len; j++) {
             if (addresses[i][j] == '1') one[j]++;
@@ -160,22 +162,23 @@ void run_zero_cost(ifstream& fin, ofstream& fout, int index_cnt, int len, int n_
     }
 
     // Debug
-    // for (int j = 0; j < len; j++) {
-    //     cout << one[j] << " " << zero[j] << endl;
+    // for (int i = 0; i < len; i++) {
+    //     cout << "one: " << one[i] << " zero: " << zero[i] << endl;
     // }
 
     // Compute Q
     for (int i = 0; i < len; i++)
-        Q[i] = min(zero[i], one[i]) / max(zero[i], one[i]);
+        Q[i] = double(min(zero[i], one[i])) / double(max(zero[i], one[i]));
 
     // Debug
+    // cout << "Quality" << endl;
     // for (int i = 0; i < len; i++)
     //     cout << Q[i] << " ";
     // cout << endl;
 
 
-    // Calculate Correlation
-    int C[len][len];
+    // Calculate Correlation (C)
+    double C[len][len];
     for (int i = 0; i < len; i++) {
         for (int j = 0 ; j < len; j++) {
             if (i == j) C[i][j] = 0;
@@ -186,68 +189,132 @@ void run_zero_cost(ifstream& fin, ofstream& fout, int index_cnt, int len, int n_
                     if(addresses[k][i] == addresses[k][j]) same++;
                     else diff++;
                 }
-                C[i][j] = min(same, diff) / max(same, diff);
+                C[i][j] = double(min(same, diff)) / double(max(same, diff));
             }
         }
     }
 
     // Debug
-    for (int i = 0; i < len; i++) {
-        for (int j = 0; j < len; j++) {
-            cout << C[i][j] << " ";
+    // cout << "correlation" << endl;
+    // for (int i = 0; i < len; i++) {
+    //     for (int j = 0; j < len; j++) {
+    //         cout << C[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // Calculate Near-optimal Index Ordering
+    set<int> indices;
+    // for (int i = 0; i < index_cnt; i++) {
+    for (int i = 0; i < index_cnt; i++) {
+        double max = -1;
+        int best_idx;
+
+        // Debug
+        // cout << "Updated Quality" << endl;
+        // for (int j = 0; j < len; j++) {
+        //     cout << Q[j] << " ";
+        // }
+        // cout << endl;
+
+        for(int j = 0; j < len; j++) {
+            if (max <= Q[j]) {
+                auto it = indices.find(j);
+                if (it == indices.end()) {  // No duplicate
+                    max = Q[j];
+                    best_idx = j;
+                }
+            }
         }
-        cout << endl;
+        for(int j = 0; j < len; j++) Q[j] *= C[j][best_idx];
+        indices.insert(best_idx);
     }
 
+    // Debug
+    // for (auto it = indices.begin(); it != indices.end(); it++)
+    //     cout << *it << " ";
+    // cout << endl;
+
+    // Calculate indices
+    vector<int> tags_sorted, indices_sorted;
+    auto it = indices.begin();
+    for (int i = len + offset_cnt - 1; i >= offset_cnt; i--) {
+        // if (it != indices.end())
+        // cout << "check " << (len + offset_cnt - 1 - (*it)) << endl;
+        if(i == (len + offset_cnt - 1 - (*it)) && it != indices.end()) {
+            indices_sorted.push_back(i);
+            it++;
+        } else tags_sorted.push_back(i);
+    }
+    print_idx(fout, "Indexing bits:", indices_sorted);
+
+    // Debug
+    cout << "tags:";
+    for (int i = 0; i < tags_sorted.size(); i++) {
+        cout << " " << tags_sorted[i];
+    }
+    cout << endl << "indices:";
+    for (int i = 0; i < indices_sorted.size(); i++) {
+        cout << " " << indices_sorted[i];
+    }
+    cout << endl;
+    
     // Execute
     int total_miss = 0;
     fout << title << endl;
-    // for (int i = 0; i < addresses.size(); i++) {
-    //     bool miss = true;
-    //     fout << addresses[i];
+    for (int i = 0; i < addresses.size(); i++) {
+        bool miss = true;
+        fout << addresses[i];
 
-        
+        string tag, index;
+        for (int j = 0; j < tags_sorted.size(); j++) {
+            tag += addresses[i][(len + offset_cnt - 1) - tags_sorted[j]];
+        }
+        for (int j = 0; j < indices_sorted.size(); j++) {
+            index += addresses[i][(len + offset_cnt - 1) - indices_sorted[j]];
+        }
 
-    //     // string tag = addresses[i].substr(0, tag_cnt);
-    //     // string index = addresses[i].substr(tag_cnt, index_cnt);
-    //     int tag_int = stoi(tag, nullptr, 2);
-    //     int index_int = stoi(index, nullptr, 2);
-    //     // cout << "tag " << tag << " " << tag_int << endl;
-    //     // cout << "index " << index << " " << index_int << endl;
+        cout << "final tag " << tag << endl;
+        cout << "final index " << index << endl;
 
-    //     // Check for hit
-    //     for(int i = 0; i < n_way; i++) {
-    //         if(caches[i][index_int].tag == tag_int) {
-    //             fout << " hit" << endl;
-    //             caches[i][index_int].NRU = false;
-    //             miss = false;
-    //         }
-    //     }
+        int tag_int = stoi(tag, nullptr, 2);
+        int index_int = stoi(index, nullptr, 2);
+        // cout << "tag " << tag << " " << tag_int << endl;
+        // cout << "index " << index << " " << index_int << endl;
 
-    //     while (miss) {
-    //         bool hit = false;
-    //         // Find first 1
-    //         for(int i = 0; i < n_way; i++) {
-    //             if(caches[i][index_int].NRU) {
-    //                 fout << " miss" << endl;
-    //                 total_miss++;
-    //                 caches[i][index_int].tag = tag_int;
-    //                 caches[i][index_int].NRU = false;
-    //                 hit = true;
-    //                 break;
-    //             }
-    //         }
+        // Check for hit
+        for(int i = 0; i < n_way; i++) {
+            if(caches[i][index_int].tag == tag_int) {
+                fout << " hit" << endl;
+                caches[i][index_int].NRU = false;
+                miss = false;
+            }
+        }
 
-    //         // All 0 -> Reset all to 1
-    //         if (!hit) {
-    //             for(int i = 0; i < n_way; i++) {
-    //                 caches[i][index_int].NRU = true;
-    //             }
-    //         } else {
-    //             break;
-    //         }
-    //     }
-    // }
+        while (miss) {
+            bool hit = false;
+            // Find first 1
+            for(int i = 0; i < n_way; i++) {
+                if(caches[i][index_int].NRU) {
+                    fout << " miss" << endl;
+                    total_miss++;
+                    caches[i][index_int].tag = tag_int;
+                    caches[i][index_int].NRU = false;
+                    hit = true;
+                    break;
+                }
+            }
+
+            // All 0 -> Reset all to 1
+            if (!hit) {
+                for(int i = 0; i < n_way; i++) {
+                    caches[i][index_int].NRU = true;
+                }
+            } else {
+                break;
+            }
+        }
+    }
     fout << ".end" << endl << endl;
     print_out(fout, "Total cache miss count: ", total_miss);
 }
@@ -285,14 +352,14 @@ int main(int argc, char *argv[]){
     int index_cnt = log2(cache_sets);
     print_out(fout, "Indexing bit count: ", index_cnt);
 
-    vector<int> index;
-    for(int i = index_cnt + offset_cnt - 1; i >= offset_cnt; i--)
-        index.push_back(i);
-    print_idx(fout, "Indexing bits:", index);
+    // vector<int> index;
+    // for(int i = index_cnt + offset_cnt - 1; i >= offset_cnt; i--)
+    //     index.push_back(i);
+    // print_idx(fout, "Indexing bits:", index);
 
     fin.open(argv[2], ios::in);
     // run_LSB(fin, fout, address_bit - (index_cnt + offset_cnt), index_cnt, associativity);
-    run_zero_cost(fin, fout, index_cnt, address_bit - offset_cnt, associativity);
+    run_zero_cost(fin, fout, index_cnt, offset_cnt, address_bit - offset_cnt, associativity);
     fin.close();
     fout.close();
 
